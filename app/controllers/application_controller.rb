9d146ca2533683f2982c2d84691637fcffcb4f6d
MatiasFernandez <mfernandez@10pines.com>
2020-09-05 17:04:16 -0300

    Add some endpoints to run concurrency tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  def affected
+    log 'Procesando /affected'
+    log_around('Request HTPP') do
+      RestClient.get('localhost:8080/slow')
+    end
+    render json: { success: true, thread: Thread.current.object_id }
+  end
+
+  def unaffected
+    log 'Procesando /unaffected'
+    render json: { success: true, thread: Thread.current.object_id }
+  end
+
+  private
+
+  def log(message)
+    Rails.logger.info "[TID: #{Thread.current.object_id}] #{message}"
+  end
+
+  def log_around(message)
+    log "ANTES DE #{message}"
+    yield
+    log "DESPUES DE #{message}"
+  end
 end
