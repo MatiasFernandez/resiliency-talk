96903866a21a56ac793541b424e55ffff8d4bcc5
MatiasFernandez <mfernandez@10pines.com>
2020-09-07 20:31:11 -0300

    Add timeout to external HTTP request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,37 @@
 class ApplicationController < ActionController::Base
   def slow_request
     log_around('Request HTTP Lenta') do
-      RestClient.get('localhost:8080/slow')
+      RestClient::Request.execute(method: :get, url: 'localhost:8080/slow', timeout: 6)
     end
-    render_response
+    render_successful_response
+    rescue RestClient::Exceptions::ReadTimeout => error
+      render_error_response(error)
   end
 
   def noop
     log_around('Operacion nula') do
       # Do nothing
     end
-    render_response
+    render_successful_response
   end
 
   def slow_calc
     log_around('Operación matemática lenta') do
       expensive_calculation
     end
-    render_response
+    render_successful_response
   end
 
   private
 
   def log(message)
-    Rails.logger.info "[TID: #{Thread.current.object_id}] #{message}"
+    Rails.logger.info "#{Time.now} - TID: #{Thread.current.object_id} - #{message}"
   end
 
   def log_around(message)
     log "Iniciando <#{message}>"
     yield
+  ensure
     log "<#{message}> finalizada"
   end
 
@@ -40,7 +43,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def render_response
-    render json: { thread: Thread.current.object_id }
+  def render_successful_response
+    render json: { success: true, thread: Thread.current.object_id }
+  end
+
+  def render_error_response(error)
+    render json: { success: false, error: error.message, thread: Thread.current.object_id }
   end
 end
