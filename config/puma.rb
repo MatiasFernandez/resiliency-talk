c47a6e31ef1a266564f4f872645279d795903f58
MatiasFernandez <mfernandez@10pines.com>
2020-09-07 19:52:08 -0300

    Simplify server creation with custom number of threads/workers. Modify script/puma_stats to support workers' stats

--- config/puma.rb
+++ config/puma.rb
@@ -4,9 +4,8 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 1 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+num_threads = ENV.fetch("THREADS") { 1 }
+threads num_threads, num_threads
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -25,20 +24,21 @@ activate_control_app 'tcp://127.0.0.1:9293', { no_token: true }
 # Create state file to use for monitoring with pumactl
 state_path 'tmp/puma.state'
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+if ENV["WORKERS"].present?
+  # Specifies the number of `workers` to boot in clustered mode.
+  # Workers are forked web server processes. If using threads and workers together
+  # the concurrency of the application would be max `threads` * `workers`.
+  # Workers do not work on JRuby or Windows (both of which do not support
+  # processes).
+  workers ENV.fetch("WORKERS")
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+  # Use the `preload_app!` method when specifying a `workers` number.
+  # This directive tells Puma to first boot the application and load code
+  # before forking the application. This takes advantage of Copy On Write
+  # process behavior so workers use less memory.
+  #
+  preload_app!
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
